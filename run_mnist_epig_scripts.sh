2dc3c2bc2d3d9c78b65227b03abba11b938ae2ed
Maksym Tretiakov <ge32jaq2@lrz-dgx-a100-003.ai.lrz.de>
2023-12-16 12:38:47 +0100

    scripts adjusting

--- run_mnist_epig_scripts.sh
+++ run_mnist_epig_scripts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
-python3 run_image_classification_epig_40k.py --seed 4 --config configs/mnist_epig.yaml --device 'cpu'
-python3 run_image_classification_epig_40k.py --seed 5 --config configs/mnist_epig.yaml --device 'cpu'
-python3 run_image_classification_epig_40k.py --seed 7 --config configs/mnist_epig.yaml --device 'cpu'
-python3 run_image_classification_epig_40k.py --seed 9 --config configs/mnist_epig.yaml --device 'cpu'
+python3 run_image_classification_epig_40k_drop_false.py --seed 4 --config configs/mnist_epig.yaml --device 'cpu'
+python3 run_image_classification_epig_40k_drop_false.py --seed 5 --config configs/mnist_epig.yaml --device 'cpu'
+python3 run_image_classification_epig_40k_drop_false.py --seed 7 --config configs/mnist_epig.yaml --device 'cpu'
+python3 run_image_classification_epig_40k_drop_false.py --seed 9 --config configs/mnist_epig.yaml --device 'cpu'
+python3 run_image_classification_epig_40k_drop_false.py --seed 11 --config configs/mnist_epig.yaml --device 'cpu'
