bcf6faeaaa1739273d5fe02b001f5d776fbd97a0
Maksym Tretiakov <ge32jaq2@lrz-dgx-a100-003.ai.lrz.de>
2024-01-06 13:53:36 +0100

    create files

---
+++ run_mnist_random_scripts_short.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+python3 run_image_classification.py --seed 3 --random_acquisition True --config configs/mnist.yaml configs/methods/laplace_post.yaml
+python3 run_image_classification.py --seed 4 --random_acquisition True --config configs/mnist.yaml configs/methods/laplace_post.yaml
+python3 run_image_classification.py --seed 5 --random_acquisition True --config configs/mnist.yaml configs/methods/laplace_post.yaml
+python3 run_image_classification.py --seed 7 --random_acquisition True --config configs/mnist.yaml configs/methods/laplace_post.yaml
+python3 run_image_classification.py --seed 9 --random_acquisition True --config configs/mnist.yaml configs/methods/laplace_post.yaml
+
+python3 run_image_classification.py --seed 3 --random_acquisition True --config configs/mnist.yaml configs/methods/laplace_post_ll.yaml
+python3 run_image_classification.py --seed 4 --random_acquisition True --config configs/mnist.yaml configs/methods/laplace_post_ll.yaml
+python3 run_image_classification.py --seed 5 --random_acquisition True --config configs/mnist.yaml configs/methods/laplace_post_ll.yaml
+python3 run_image_classification.py --seed 7 --random_acquisition True --config configs/mnist.yaml configs/methods/laplace_post_ll.yaml
+python3 run_image_classification.py --seed 9 --random_acquisition True --config configs/mnist.yaml configs/methods/laplace_post_ll.yaml
