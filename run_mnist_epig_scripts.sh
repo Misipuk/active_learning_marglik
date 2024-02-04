bd202059bd8f53d6ddb0e366e1c9b70fe0f5fabd
Maksym Tretiakov <ge32jaq2@cpu-007.ai.lrz.de>
2024-02-04 12:14:42 +0100

    random seed fix in run scripts

--- run_mnist_epig_scripts.sh
+++ run_mnist_epig_scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
+python3 run_image_classification_epig_40k.py --seed 3 --config configs/mnist_epig.yaml --device 'cpu'
 python3 run_image_classification_epig_40k.py --seed 4 --config configs/mnist_epig.yaml --device 'cpu'
 python3 run_image_classification_epig_40k.py --seed 5 --config configs/mnist_epig.yaml --device 'cpu'
 python3 run_image_classification_epig_40k.py --seed 7 --config configs/mnist_epig.yaml --device 'cpu'
 python3 run_image_classification_epig_40k.py --seed 9 --config configs/mnist_epig.yaml --device 'cpu'
-python3 run_image_classification_epig_40k.py --seed 10 --config configs/mnist_epig.yaml --device 'cpu'
