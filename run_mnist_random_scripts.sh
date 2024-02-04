7b9529ca97074822681b65c3ccf94406c8dedd8b
Maksym Tretiakov <ge32jaq2@cpu-007.ai.lrz.de>
2024-02-04 11:35:13 +0100

    run scripts added

--- run_mnist_random_scripts.sh
+++ run_mnist_random_scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
-python3 run_image_classification.py --seed 3 --random_acquisition True --config configs/mnist.yaml
-python3 run_image_classification.py --seed 4 --random_acquisition True --config configs/mnist.yaml
-python3 run_image_classification.py --seed 5 --random_acquisition True --config configs/mnist.yaml
-python3 run_image_classification.py --seed 7 --random_acquisition True --config configs/mnist.yaml
-python3 run_image_classification.py --seed 9 --random_acquisition True --config configs/mnist.yaml
+python3 run_image_classification_epig_40k.py --seed 3 --random_acquisition --config configs/mnist_epig.yaml --device 'cpu'
+python3 run_image_classification_epig_40k.py --seed 4 --random_acquisition --config configs/mnist_epig.yaml --device 'cpu'
+python3 run_image_classification_epig_40k.py --seed 5 --random_acquisition --config configs/mnist_epig.yaml --device 'cpu'
+python3 run_image_classification_epig_40k.py --seed 7 --random_acquisition --config configs/mnist_epig.yaml --device 'cpu'
+python3 run_image_classification_epig_40k.py --seed 9 --random_acquisition --config configs/mnist_epig.yaml --device 'cpu'
