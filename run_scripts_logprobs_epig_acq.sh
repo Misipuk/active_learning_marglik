59b65752c1d0ef88910adc4c0871074281b27299
Maksym Tretiakov <ge32jaq2@p100-001.ai.lrz.de>
2024-02-09 23:42:32 +0100

    batch size fix

--- run_scripts_logprobs_epig_acq.sh
+++ run_scripts_logprobs_epig_acq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
-python3 run_acq_epig_marglik_40k.py --seed 3 --config configs/mnist_tuned_burning_epig.yaml
-python3 run_acq_epig_marglik_40k.py --seed 4 --config configs/mnist_tuned_burning_epig.yaml
-python3 run_acq_epig_marglik_40k.py --seed 5 --config configs/mnist_tuned_burning_epig.yaml
-python3 run_acq_epig_marglik_40k.py --seed 7 --config configs/mnist_tuned_burning_epig.yaml
-python3 run_acq_epig_marglik_40k.py --seed 9 --config configs/mnist_tuned_burning_epig.yaml
+python3 run_acq_epig_marglik_40k.py --seed 3 --config configs/mnist_tuned_burning_epig_batch.yaml
+python3 run_acq_epig_marglik_40k.py --seed 5 --config configs/mnist_tuned_burning_epig_batch.yaml
+python3 run_acq_epig_marglik_40k.py --seed 7 --config configs/mnist_tuned_burning_epig_batch.yaml
+python3 run_acq_epig_marglik_40k.py --seed 9 --config configs/mnist_tuned_burning_epig_batch.yaml
+python3 run_acq_epig_marglik_40k.py --seed 10 --config configs/mnist_tuned_burning_epig_batch.yaml
